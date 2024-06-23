c4e70b73042cebb4611bcbd35c4744a5ebd1bd82
gybran nauval <nauvalgybran@gmail.com>
2024-06-23 17:17:59 +0700

    create homepage

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,19 +1,211 @@
+import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:go_restoran/const.dart';
+import 'package:go_restoran/model/food/model_get_food.dart';
+import 'package:go_restoran/screen/food/detail_food_page.dart';
+import 'package:go_restoran/screen/food/list_food_page.dart';
+import 'package:go_restoran/screen/user/profil_page.dart';
+import 'package:http/http.dart' as http;
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<Homepage> createState() => _HomepageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomePageState extends State<HomePage> {
+  final List<Map<String, String>> _imageDescriptions = [
+    {
+      'image': 'assets/slide1.jpg',
+    },
+    {
+      'image': 'assets/slide2.jpg',
+    },
+    {
+      'image': 'assets/slide3.jpg',
+    },
+  ];
+
+  bool isLoading = false;
+  late List<Food> _productList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getProduct();
+  }
+
+  Future<void> getProduct() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      final res = await http.get(Uri.parse('${url}get_food.php'));
+
+      if (res.statusCode == 200) {
+        final modelProduct = modelGetFoodFromJson(res.body);
+        setState(() {
+          _productList = modelProduct.foods ?? [];
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed Load data')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.toString())));
+    } finally {
+      setState(() => isLoading = false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("homepage"),
-      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 90,
+                        child: Image.asset(
+                          'assets/logo2.png',
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.person,
+                            size: 40,
+                          ))
+                    ],
+                  ),
+                ),
+                CarouselSlider(
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    autoPlayAnimationDuration: Duration(seconds: 1),
+                  ),
+                  items: _imageDescriptions.map((item) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 5, vertical: 5),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          item['image']!,
+                          height: 200,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Makanan",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ListFoodPage()));
+                        },
+                        child: Text("See all",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Container(
+                    height: 186,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 5,
+                        itemBuilder: (context, index) {
+                          Food data = _productList[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          DetailFoodPage(data)));
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 10),
+                              child: Container(
+                                height: 170,
+                                width: 120,
+                                decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        width: 120,
+                                        height: 140,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          child: Image.network(
+                                            '${urlImg}${data.image}', // Assuming Product has an imageUrl field
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          data.name,
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                ),
+              ],
+            ),
+      // Category
     );
   }
 }
