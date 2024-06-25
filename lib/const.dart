e8fbe64170fd066c7beefe302f16fa3838cc2b1a
gybran nauval <nauvalgybran@gmail.com>
2024-06-25 10:27:24 +0700

    minor changes

--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,4 @@
-const ipaddr = '192.168.100.54';
-// const ipaddr = '10.127.218.148';
+// const ipaddr = '192.168.100.54';
+const ipaddr = '10.127.237.151';
 const url = "http://$ipaddr/project_uas/api/";
-const urlImg = "http://$ipaddr/project_uas/api/";
+const urlImg = "http://$ipaddr/project_uas/api/images/";
